4947345d5927133c84ccb4ea98026aabc7057d60
graysonLL <liam.m.a.jones@gmail.com>
2024-05-06 16:56:18 +0800

    added login and logout saving on app close

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { createContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext();
 
@@ -7,14 +8,33 @@ export const AuthProvider = ({ children }) => {
   const [userToken, setUserToken] = useState(null);
 
   const login = () => {
+    setIsLoading(true);
     setUserToken("liamjones");
+    AsyncStorage.setItem("userToken", "liamjones");
     setIsLoading(false);
   };
 
   const logout = () => {
+    setIsLoading(true);
     setUserToken(null);
+    AsyncStorage.removeItem("userToken");
     setIsLoading(false);
   };
+
+  const isLoggedIn = async () => {
+    try {
+      setIsLoading(true);
+      let userToken = await AsyncStorage.getItem("userToken");
+      setUserToken(userToken);
+      setIsLoading(false);
+    } catch (e) {
+      console.log(`isLogged in error ${e}`);
+    }
+  };
+
+  useEffect(() => {
+    isLoggedIn();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ login, logout, isLoading, userToken }}>
