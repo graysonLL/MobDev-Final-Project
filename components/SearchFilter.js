6922e0c19f8d861b8f95f2b72880c10b9ad6d744
graysonLL <liam.m.a.jones@gmail.com>
2024-05-08 03:26:55 +0800

    search changed

--- components/SearchFilter.js
+++ components/SearchFilter.js
@@ -9,89 +9,38 @@ import {
 } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import SearchStyles from "../styles/SearchStyles";
 
-const SearchFilter = ({ data, input, setInput }) => {
+const SearchFilter = ({ data, input, setInput, selectedCategory }) => {
   const navigation = useNavigation();
-  const { width } = Dimensions.get("window");
-  const margin = 10;
-  const itemWidth = (width - margin * 2) / 2;
+  const filteredShoes = data.filter((item) => {
+    const categoryMatch =
+      !selectedCategory || selectedCategory === item.category;
+    const inputMatch =
+      !input || item.title.toLowerCase().includes(input.toLowerCase());
+    return categoryMatch && inputMatch;
+  });
 
   return (
-    <View style={[styles.outerContainer]}>
-      <FlatList
-        data={data}
-        contentContainerStyle={{ paddingBottom: 100 }}
-        renderItem={({ item }) => {
-          if (input === "") {
-            return (
-              <TouchableOpacity
-                style={[styles.itemContainer]}
-                onPress={() => navigation.navigate("Product", item)}
-              >
-                <Text style={styles.itemTitle}>{item.title}</Text>
-                <Image source={item.image} style={styles.itemImage} />
-                <Text style={styles.itemPrice}>{item.price}</Text>
-                <Text style={styles.itemBody}>{item.body}</Text>
-              </TouchableOpacity>
-            );
-          }
-
-          if (item.title.toLowerCase().includes(input.toLowerCase())) {
-            return (
-              <TouchableOpacity
-                style={[styles.itemContainer]}
-                onPress={() => navigation.navigate("Product", item)}
-              >
-                <Text style={styles.itemTitle}>{item.title}</Text>
-                <Image source={item.image} style={styles.itemImage} />
-                <Text style={styles.itemPrice}>{item.price}</Text>
-                <Text style={styles.itemBody}>{item.body}</Text>
-              </TouchableOpacity>
-            );
-          }
-        }}
-      />
-    </View>
+    <FlatList
+      data={filteredShoes}
+      contentContainerStyle={{ paddingBottom: 70 }}
+      numColumns={2}
+      style={SearchStyles.listContainer}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          style={SearchStyles.itemContainer}
+          onPress={() => navigation.navigate("Product", item)}
+        >
+          <Text style={SearchStyles.itemTitle}>{item.title}</Text>
+          <Image source={item.image} style={SearchStyles.itemImage} />
+          <Text style={SearchStyles.itemPrice}>{item.price}</Text>
+          <Text style={SearchStyles.itemBody}>{item.body}</Text>
+          <Text>wtf</Text>
+        </TouchableOpacity>
+      )}
+    />
   );
 };
 
 export default SearchFilter;
-
-const styles = StyleSheet.create({
-  outerContainer: {
-    width: "100%",
-  },
-  container: {
-    flex: 1,
-    padding: 10,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  itemContainer: {
-    marginBottom: 5,
-    padding: 10,
-    backgroundColor: "#f0f0f0",
-    borderRadius: 5,
-    alignItems: "center",
-    justifyContent: "center",
-    height: "auto",
-    width: "100%",
-  },
-  itemTitle: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  itemPrice: {
-    fontSize: 14,
-    color: "gray",
-  },
-  itemBody: {
-    fontSize: 12,
-    color: "gray",
-  },
-  itemImage: {
-    width: 150, 
-    height: 100, 
-    marginVertical: 10,
-  },
-});
