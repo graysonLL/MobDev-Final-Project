a4a9290fdd412eac403a1b7442e0e85afc523f17
Eduardo <edelcortes3@gmail.com>
2024-05-20 22:41:37 +0800

    splash

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AppNav } from './navigation/AppNav';
 import { AuthProvider } from './context/AuthContext';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import SplashScreen from './screens/SplashScreenView';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <AuthProvider>
-        <AppNav />
-      </AuthProvider>
-    </GestureHandlerRootView>
-  );
+
+  const [isShowSplash, setIsShowSplash] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShowSplash(false);
+    }, 3000);
+  });
+
+
+
+  return <>
+    {isShowSplash ? <SplashScreen /> :
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <AuthProvider>
+          <AppNav />
+        </AuthProvider>
+      </GestureHandlerRootView>
+    }
+  </>
 }
